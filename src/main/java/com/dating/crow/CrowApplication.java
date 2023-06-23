16ba509826404047217a93c895e2dfa43607b994
Manasse Nkundimana <promanasse@gmail.com>
2023-06-23 17:29:38 -0400

    third commit

--- src/main/java/com/dating/crow/CrowApplication.java
+++ src/main/java/com/dating/crow/CrowApplication.java
@@ -1,13 +1,26 @@
 package com.dating.crow;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CrowApplication {
+import com.dating.crow.profile.photo.FilesStorageService;
 
+import jakarta.annotation.Resource;
+
+@SpringBootApplication
+public class CrowApplication implements CommandLineRunner{
+
+	  @Resource
+	  FilesStorageService storageService;
+	  
 	public static void main(String[] args) {
 		SpringApplication.run(CrowApplication.class, args);
 	}
 
+	 @Override
+	  public void run(String... arg) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	  }
 }
