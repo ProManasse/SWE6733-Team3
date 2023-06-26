7457e2b18b1b01aee394f7916ae72bb62f98d33f
Manasse Nkundimana <promanasse@gmail.com>
2023-06-26 09:46:33 -0400

    Setup Completete Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import('./authentication/auth.module').then(m=>m.AuthModule)
+  },
+  {
+    path:'dating',
+    loadChildren:()=>import('./dater/dating.module').then(m=>m.DatingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
